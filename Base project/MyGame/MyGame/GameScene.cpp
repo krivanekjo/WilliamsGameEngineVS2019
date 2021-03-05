bd729a88b6228c87a819c77fc8e8df06c342e92b
Jordan Krivanek <krivanekj@rsd407.org>
2021-03-05 13:10:24 -0800

    I made flying asteroids

--- Base project/MyGame/MyGame/GameScene.cpp
+++ Base project/MyGame/MyGame/GameScene.cpp
@@ -1,8 +1,12 @@
 #include "GameScene.h"
 #include "Spaceship.h"
+#include "MeteorSpawner.h"
 
 GameScene::GameScene() 
 {
 	SpaceshipPtr spaceship = std::make_shared<Spaceship>();
 	addGameObject(spaceship);
+
+	MeteorSpawnerPtr meteorSpawner = std::make_shared<MeteorSpawner>();
+	addGameObject(meteorSpawner);
 }
